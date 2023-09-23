288723159355cf4f4c9e81ca780101761eb6b16e
alpaca-tc <alpaca-tc@alpaca.tc>
2023-09-23 22:55:33 +0900

    UIの変更に追随する

--- api/niigata_jp/mod.ts
+++ api/niigata_jp/mod.ts
@@ -1,6 +1,10 @@
-import { DOMParser, Element, Node } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
-import { Feed } from 'https://jspm.dev/feed'
-import { urlParse } from 'https://deno.land/x/url_parse/mod.ts';
+import {
+  DOMParser,
+  Element,
+  Node,
+} from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+import { Feed } from "https://jspm.dev/feed";
+import { urlParse } from "https://deno.land/x/url_parse/mod.ts";
 
 class Entry {
   constructor(
@@ -8,50 +12,56 @@ class Entry {
     public title: string,
     public updated: Date,
     public content: string,
-    public link: string
+    public link: string,
   ) {}
 }
 
-const BASE_URL = "https://www.city.sado.niigata.jp/site/ijyu/5419.html"
-const FEED_URL = 'spaceflightnow.com'
+const BASE_URL = "https://www.city.sado.niigata.jp/site/ijyu/5419.html";
+const FEED_URL = "spaceflightnow.com";
 
 export const generateRss2 = async (): Promise<string> => {
-  const pageResponse = await fetch(BASE_URL)
-  const pageBody = await pageResponse.text()
+  const pageResponse = await fetch(BASE_URL);
+  const pageBody = await pageResponse.text();
 
-  const pageDom = new DOMParser().parseFromString(pageBody, "text/html")!
-  const h2Doms = Array.from(pageDom.querySelectorAll('h2'))
-  const newArrival = Array.from(h2Doms).find((element) => (element as Element).innerText == '新着情報') as Element
+  const pageDom = new DOMParser().parseFromString(pageBody, "text/html")!;
+  const h2Doms = Array.from(pageDom.querySelectorAll("h2"));
+  const newArrival = Array.from(h2Doms).find((element) =>
+    (element as Element).innerText == "新着情報【新規掲載】"
+  ) as Element;
 
-  const pDoms = Array.from(newArrival.parentElement!.querySelectorAll('p > a'))
+  const pDoms = Array.from(newArrival.parentElement!.querySelectorAll("p > a"));
   const entries = pDoms.map((element) => {
-    const text = (element as Element).innerText
-    const [, yearStr, monthStr, dayStr, title] = (/(\d{4})年(\d+)月(\d+)日\s*(.*)$/.exec(text) || ['', '', '', '', ''])
-    const [year, month, day] = [yearStr, monthStr, dayStr].map((val) => parseInt(val, 10))
-    const date = new Date(year, month, day)
+    const text = (element as Element).innerText;
+    const [, yearStr, monthStr, dayStr, title] =
+      /(\d{4})年(\d+)月(\d+)日\s*(.*)$/.exec(text) || ["", "", "", "", ""];
+    const [year, month, day] = [yearStr, monthStr, dayStr].map((val) =>
+      parseInt(val, 10)
+    );
+    const date = new Date(year, month, day);
 
-    const href = urlParse(BASE_URL)
-    href.pathname = (element as Element).getAttribute('href') || ''
+    const href = urlParse(BASE_URL);
+    href.pathname = (element as Element).getAttribute("href") || "";
 
-    return new Entry(text, title, date, text, href.toString())
-  })
+    return new Entry(text, title, date, text, href.toString());
+  });
 
   const responseFeed = new Feed({
-    title: pageDom.querySelector('title')!.innerText,
-    description: pageDom.querySelector('meta[name="description"]')!.getAttribute('content'),
+    title: pageDom.querySelector("title")!.innerText,
+    description: pageDom.querySelector('meta[name="description"]')!
+      .getAttribute("content"),
     link: FEED_URL,
-    updated: entries[0]!.updated
-  })
+    updated: entries[0]!.updated,
+  });
 
   entries.forEach((entry) => {
     responseFeed.addItem({
       title: entry.title,
       id: entry.id,
       link: entry.link,
-      description: '',
-      date: entry.updated
-    })
-  })
+      description: "",
+      date: entry.updated,
+    });
+  });
 
-  return responseFeed.rss2()
-}
+  return responseFeed.rss2();
+};
